cc1d0da525c173cdac0de4a9bf5834944922b509
nanaya <me@nanaya.net>
2024-11-14 16:41:27 +0900

    Test for error on null json body

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Psr\Http\Message\ServerRequestInterface;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::post('/', function () {
+    return ['request' => app()->make(ServerRequestInterface::class)->getParsedBody()];
 });
